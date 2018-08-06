282a45f276f8fce4ea29c77a593eeadf6f18747c
Josh Bendavid <Josh.Bendavid@cern.ch>
2018-08-06 03:10:01 +0000

    cleanup unused code

--- python/tfsparseutils.py
+++ python/tfsparseutils.py
@@ -39,49 +39,6 @@ def makeCache(x):
   tf.add_to_collection('cache_initializers', it.initializer)
   return it.get_next()
 
-def flatten_indices(indices,shape):
-  if indices.dtype != tf.int32:
-    indices = tf.cast(indices,tf.int32)
-  if len(shape) == 1:
-    indicesflat = tf.squeeze(indices,-1)
-    return (indicesflat, int(shape[0]))
-  flat_shape = 1
-  for s in shape:
-    flat_shape *= int(s)
-  flat_shape = (flat_shape,)
-  idxmodifier = tf.cumprod(shape, exclusive=True, reverse=True)
-  idxmodifier = tf.expand_dims(idxmodifier,-1)
-  indicesflat = tf.matmul(indices,idxmodifier)
-  indicesflat = tf.squeeze(indicesflat,-1)
-  return (indicesflat, flat_shape[0])
-
-def sparse_reduce_sum_0(in_sparse, ndims=1, reduced_shape=None):
-  reduced_shape = in_sparse.dense_shape[:-ndims]
-  print(reduced_shape)
-  indicespartial = in_sparse.indices[:,ndims:]
-  indicesflat,flat_size = flatten_indices(indicespartial, reduced_shape)
-  
-  segment_ids = indicesflat
-  
-  reduced_values = tf.unsorted_segment_sum(in_sparse.values, segment_ids,flat_size)
-  reduced = tf.reshape(reduced_values, reduced_shape)
-  return reduced
-
-def simple_sparse_slice(in_sparse, start, end):
-  validindices = tf.logical_and(tf.greater_equal(in_sparse.indices,start), tf.less(in_sparse.indices, end))
-  validindices = tf.reduce_all(validindices,axis=-1)
-  validindices = tf.where(validindices)
-  validindices = tf.squeeze(validindices,-1)
-  
-  out_indices = tf.gather(in_sparse.indices,validindices)
-  out_indices = out_indices - start
-  out_values = tf.gather(in_sparse.values,validindices)
-  out_shape = []
-  for s,e in zip(start,end):
-    out_shape.append(e-s)
-  
-  return SimpleSparseTensor(out_indices,out_values,out_shape)
-
 def simple_sparse_slice0begin(in_sparse, begin, doCache = False):
 
   out_shape = [in_sparse.dense_shape[0] - begin] + list(in_sparse.dense_shape)[1:]
